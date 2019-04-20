24db84f9930808c09f00c0df72fb6e1318bc752a
Gustavo Martins do Santos <gustavoms97@hotmail.com>
2019-04-20 16:47:44 -0300

    Correção no index.

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
-
 const app = express();
-
+//Configures express middlewares.
 app.use(morgan('dev'));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/files', express.static(path.resolve(__dirname, 'tmp')));
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
 require('./app/controllers/index')(app);
-
 app.listen(3000);
